3329e9696901b284d308b624431abb86b90905ad
Ondřej Doněk <ondrejd@gmail.com>
2018-03-26 08:12:52 +0200

    Initial version.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Customizations for WP Contacts plugin
+
+[WordPress][1] plugin that changes behavior of [WP Contacts][2] plugin in way which one of my clients wanted.
+
+## Features
+
+- own frontend page template
+
+[1]: https://wordpress.net
+[2]: http://wpcontacts.co/
---
+++ odwp-shwcp_gleton.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Customizations for WP Contacts plugin
+ * Plugin URI: https://github.com/ondrejd/odwp-shwcp_gleton
+ * Description: Plugin that changes behavior of <a href="http://wpcontacts.co/" target="blank">WP Contacts</a> plugin in way which one of my clients wanted.
+ * Version: 0.0.1
+ * Author: Ondřej Doněk
+ * Author URI: https://ondrejd.com/
+ * License: GPLv3
+ * Requires at least: 4.8
+ * Tested up to: 4.8.4
+ * Tags: debug,log,development
+ * Donate link: https://www.paypal.me/ondrejd
+ *
+ * Text Domain: odwpglt
+ * Domain Path: /languages/
+ *
+ * @author Ondřej Doněk <ondrejd@gmail.com>
+ * @link https://github.com/ondrejd/odwp-shwcp_gleton for the canonical source repository
+ * @package odwp-shwcp_gleton
+ * @since 0.0.1
+ */
+
+if( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
